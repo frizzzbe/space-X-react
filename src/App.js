4b075094cb1391e409ff83ea8439c6151016fbdb
Arkadiy <frizzzbe@yandex.ru>
2020-10-16 18:39:34 +0400

    Загрузка струткуры сайта и базовой логики для переключения между страницами

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import Header from './components/header/header.js';
+import Main from './components/main/Main.js';
+import Features from './components/features/features.js';
+import Footer from './components/footer/footer.js';
+// import Details from './components/details/details.js';
+// import Calendar from './components/calendar/calendar.js';
+import FetchData from './service/FetchData.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  fetchData = new FetchData();
+
+  state = {
+    rocket: "Falcon 1",
+    rocketFeatures: null,
+    rockets: [],
+  };
+
+  componentDidMount() {
+    this.updateRocket();
+    // this.fetchData.getRocket().then(data => console.log(data));
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket()
+    .then(data => {
+      this.setState({ rockets: data.map(item => item.name) });
+      return data;
+    })
+    .then(data => {data.find(item => item.name === this.state.rocket)})
+    .then(rocketFeatures => this.setState({ rocketFeatures }));
+  }
+
+  changeRocket = (rocket) => {
+    this.setState({
+      rocket
+    }, this.updateRocket());
+  }
+
+  render(){
+    return (
+      <>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket}/> 
+        <Main rocket={this.state.rocket}/>
+        <Features/>
+        <Footer/>
+      </>
+    );
+  }
 }
 
 export default App;
